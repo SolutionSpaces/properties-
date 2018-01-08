405d234decdd621e696a19be6eb299db8a486702
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-01-09 00:26:00 +0100

    Adjust it to output of mailer

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 const Apify = require('apify');
 const puppeteer = require('puppeteer');
-const typeCheck = require('type-check').typeCheck;
-
-// Definition of the input
-const INPUT_TYPE = `{
-    target: String,
-}`;
 
 async function extractText(page, selector) {
     try {
@@ -28,13 +22,8 @@ async function extractNumber(page, selector) {
 
 Apify.main(async () => {
     const input = await Apify.getValue('INPUT');
-    if (!typeCheck(INPUT_TYPE, input)) {
-        console.log('Expected input:');
-        console.log(INPUT_TYPE);
-        console.log('Received input:');
-        console.dir(input);
-        throw new Error('Received invalid input');
-    }
+    const keys = Object.keys(input).filter((name) => /link_url/.test(name));
+    const links = keys.reduce(function (a, k) { a.push(input[k]); return a; }, [])
 
     const browser = await puppeteer.launch({
         args: [
@@ -44,60 +33,59 @@ Apify.main(async () => {
     });
 
     const page = await browser.newPage();
-    await page.goto(input.target, { waitUntil: 'networkidle2' });
+    const output = await links.reduce(async (previousPromise, link) => {
+        const collection = await previousPromise;
+        await page.goto(link, { waitUntil: 'networkidle2' });
 
-    const title = await extractText(page, '#expose-title');
-    const price = await extractNumber(page, '.is24qa-kaltmiete');
-    const rooms = await extractText(page, '.is24qa-zi');
-    const size = await extractNumber(page, '.is24qa-flaeche');
-    const attributes = await extractText(page, '.boolean-listing');
-    const proximity = await extractText(page, '.is24qa-lage');
-    const facilities = await extractText(page, '.is24qa-ausstattung');
-    const description = await extractText(page, '.is24qa-objektbeschreibung');
-    const misc = await extractText(page, '.is24qa-sonstiges');
-    const utilities = await extractNumber(page, '.is24qa-nebenkosten');
-    const heatingCosts = await extractNumber(page, '.is24qa-heizkosten');
-    const totalPrice = await extractNumber(page, '.is24qa-gesamtmiete');
-    const ids = await extractText(page, '.is24-scoutid__content');
-    const address = await extractText(page, '.address-block');
-    const available = await extractText(page, '.is24qa-bezugsfrei-ab');
-    const buildingType = await extractText(page, '.is24qa-typ');
-    const buildYear = await extractText(page, '.is24qa-baujahr');
-    const condition = await extractText(page, '.is24qa-objektzustand');
-    const heating = await extractText(page, '.is24qa-heizungsart');
-    const energySource = await extractText(page, '.is24qa-wesentliche-energietraeger');
-    const images = await page.$$eval('img.sp-image', imgs => imgs.map((img) => img.getAttribute('data-src')));
+        const title = await extractText(page, '#expose-title');
+        const price = await extractNumber(page, '.is24qa-kaltmiete');
+        const rooms = await extractText(page, '.is24qa-zi');
+        const size = await extractNumber(page, '.is24qa-flaeche');
+        const attributes = await extractText(page, '.boolean-listing');
+        const proximity = await extractText(page, '.is24qa-lage');
+        const facilities = await extractText(page, '.is24qa-ausstattung');
+        const description = await extractText(page, '.is24qa-objektbeschreibung');
+        const misc = await extractText(page, '.is24qa-sonstiges');
+        const utilities = await extractNumber(page, '.is24qa-nebenkosten');
+        const heatingCosts = await extractNumber(page, '.is24qa-heizkosten');
+        const totalPrice = await extractNumber(page, '.is24qa-gesamtmiete');
+        const ids = await extractText(page, '.is24-scoutid__content');
+        const address = await extractText(page, '.address-block');
+        const available = await extractText(page, '.is24qa-bezugsfrei-ab');
+        const buildingType = await extractText(page, '.is24qa-typ');
+        const buildYear = await extractText(page, '.is24qa-baujahr');
+        const condition = await extractText(page, '.is24qa-objektzustand');
+        const heating = await extractText(page, '.is24qa-heizungsart');
+        const energySource = await extractText(page, '.is24qa-wesentliche-energietraeger');
+        const images = await page.$$eval('img.sp-image', imgs => imgs.map((img) => img.getAttribute('data-src')));
 
+        collection.push({
+            url: link,
+            ids: ids,
+            title: title,
+            price: price,
+            rooms: rooms,
+            size: size,
+            attributes: attributes.replace('/ ', '/').split(' '),
+            proximity: proximity,
+            facilities: facilities,
+            description: description,
+            misc: misc,
+            utilities: utilities,
+            heatingCosts: heatingCosts,
+            totalPrice: totalPrice,
+            address: address,
+            available: available,
+            buildingType: buildingType,
+            buildYear: buildYear,
+            condition: condition,
+            heating: heating,
+            energySource: energySource,
+            images: images
+        })
+        return collection
+    }, Promise.resolve([]))
     browser.close();
-
-    console.log(price)
-
-    // Store the output
-    const output = {
-        url: input.target,
-        ids: ids,
-        title: title,
-        price: price,
-        rooms: rooms,
-        size: size,
-        attributes: attributes.replace('/ ', '/').split(' '),
-        proximity: proximity,
-        facilities: facilities,
-        description: description,
-        misc: misc,
-        utilities: utilities,
-        heatingCosts: heatingCosts,
-        totalPrice: totalPrice,
-        address: address,
-        available: available,
-        buildingType: buildingType,
-        buildYear: buildYear,
-        condition: condition,
-        heating: heating,
-        energySource: energySource,
-        images: images
-    };
-
     console.log(output)
     await Apify.setValue('OUTPUT', output)
 });
